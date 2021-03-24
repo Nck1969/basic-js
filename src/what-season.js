a6aa5147b339840874b9ec8a680042a65f749fc7
Nck1969 <56834272+Nck1969@users.noreply.github.com>
2021-03-25 00:08:08 +0300

    fix fix func date.getMonth()

--- src/what-season.js
+++ src/what-season.js
@@ -3,8 +3,8 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function getSeason( date ) {
   if ( typeof(date) != 'string' ) throw Error;
   if ( date == undefined ) return 'Unable to determine the time of year!'
-  if( date.getMonth == 11 || date.getMonth == 0 || date.getMonth == 1) return 'winter' 
-  if( date.getMonth == 2 || date.getMonth == 3 || date.getMonth == 4) return 'spring'
-  if( date.getMonth == 5 || date.getMonth == 6 || date.getMonth == 7) return 'summer'
-  if( date.getMonth == 8 || date.getMonth == 9 || date.getMonth == 10) return 'autumn'
+  if( date.getMonth() == 11 || date.getMonth() == 0 || date.getMonth() == 1) return 'winter' 
+  if( date.getMonth() == 2 || date.getMonth() == 3 || date.getMonth() == 4) return 'spring'
+  if( date.getMonth() == 5 || date.getMonth() == 6 || date.getMonth() == 7) return 'summer'
+  if( date.getMonth() == 8 || date.getMonth() == 9 || date.getMonth() == 10) return 'autumn'
 };
