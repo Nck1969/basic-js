490af0be431da46e95f6ec86e7020ceef8a015fc
Nck1969 <56834272+Nck1969@users.noreply.github.com>
2021-03-24 23:45:48 +0300

    fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,18 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(sampleActivity) {
-  
+module.exports = function dateSample( sampleActivity ) {
   let regex = /[A-Za-z\W]/;
-    if (sampleActivity == '0' || sampleActivity == '') return false;
-    if(typeof(sampleActivity) != 'string') return false;
-    if ( sampleActivity.search(regex) !=  -1)  return false;
+  if (sampleActivity == '0' || sampleActivity == '') return false;
+  if(typeof(sampleActivity) != 'string') return false;
+  if ( sampleActivity.search(regex) !=  -1)  return false;
 
-    sampleActivity = Number(sampleActivity);
+  sampleActivity = Number(sampleActivity);
 
-    let year = 0;
+  let year = 0;
 
-    year = ( (Math.log(MODERN_ACTIVITY/sampleActivity)) / ( ( 0.693 / HALF_LIFE_PERIOD )))
-    year = Math.ceil(year);
+  year = ( (Math.log(MODERN_ACTIVITY/sampleActivity)) / ( ( 0.693 / HALF_LIFE_PERIOD )))
+  year = Math.ceil(year);
   return year;
 };
