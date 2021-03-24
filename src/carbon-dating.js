2ece88fc17146bfb45f0f0bfbaeb1ebb275a4282
Nck1969 <56834272+Nck1969@users.noreply.github.com>
2021-03-24 22:31:44 +0300

    fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,16 +4,17 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
+  
   let regex = /[A-Za-z\W]/;
-  if (sampleActivity == '0' || sampleActivity == '') return false;
-  if(typeof(sampleActivity) != 'string') return false;
-if ( sampleActivity.search(regex) !=  -1)  return false;
+    if (sampleActivity == '0' || sampleActivity == '') return false;
+    if(typeof(sampleActivity) != 'string') return false;
+    if ( sampleActivity.search(regex) !=  -1)  return false;
 
-  sampleActivity = Number(sampleActivity);
+    sampleActivity = Number(sampleActivity);
 
-  let year = 0;
+    let year = 0;
 
-  year = ( (Math.log(MODERN_ACTIVITY/sampleActivity)) / ( ( 0.693 / HALF_LIFE_PERIOD )))
-  year = Math.ceil(year);
- return year;
+    year = ( (Math.log(MODERN_ACTIVITY/sampleActivity)) / ( ( 0.693 / HALF_LIFE_PERIOD )))
+    year = Math.ceil(year);
+  return year;
 };
