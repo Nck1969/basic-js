8216ee9ed641a6e602b69e450bee8ea3ca365cce
Nck1969 <56834272+Nck1969@users.noreply.github.com>
2021-03-26 00:31:43 +0300

    docs write code for task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  if( typeof(arr) != 'object'){
+    throw Error;
+  }
+  let arr2 = [];    
+  
+  let temp1 = '--double-next';
+  let temp2 = '--double-prev';
+  let temp3 = '--diskard-next';
+  let temp4 = '--diskard-prev';
+  
+  for( let i = 0; i < arr.length; i++){
+    if( arr[i] != temp1 && arr[i] != temp2 && arr[i] != temp3 && arr[i] != temp4){
+      arr2.push(arr[i]);
+    }
+    else if( arr[i] == temp1){
+      if( arr[i+1] != undefined ){
+        arr2.push(arr[i+1]);
+      }
+    }
+    else if( arr[i] == temp2){
+      if( arr[i-1] != undefined ){
+        arr2.push(arr[i-1]);
+        arr2.push(arr[i-1]);
+      }
+    }
+    else if( arr[i] == temp3){
+    i++;
+    }
+    else if( arr[i] == temp4){
+      arr2.pop();
+    }
+  }
+  
+  return arr2;
 };
